25f73cf2a767ff3ad48169486b1f74b05ef7c39b
jsaliba <justin.saliba@ricston.com>
2016-05-10 09:24:01 +0200

    Submit failing test

---
+++ src/test/java/bloop/TestLol.java
@@ -0,0 +1,35 @@
+package bloop;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+
+import org.apache.commons.io.IOUtils;
+import org.junit.Test;
+import org.mule.api.MuleEvent;
+import org.mule.tck.junit4.FunctionalTestCase;
+
+public class TestLol extends FunctionalTestCase {
+
+	@Override
+	protected String getConfigFile() {
+		return "bloop.xml";
+	}
+	
+	@Test
+	public void testLol() throws IOException, Exception {
+		assertTrue(true);
+//		runFlowAndExpect("bloopFlow", IOUtils.toString(getClass().getResourceAsStream("lol.html")));
+	}
+	
+	@Test
+	public void testContentType() throws Exception {
+		MuleEvent event = runFlow("bloopFlow");
+		String payload = (String) event.getMessage().getPayload();
+		assertEquals(IOUtils.toString(getClass().getResourceAsStream("lol.html")), payload);
+		
+		assertEquals("text/html", event.getMessage().getOutboundProperty("Content-Type"));
+	}
+	
+}
